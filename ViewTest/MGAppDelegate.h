19df4b654ee120e78fee16d048e65502c9504580
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-09-11 12:31:29 -0700

    Initial Commit

---
+++ ViewTest/MGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MGAppDelegate.h
+//  ViewTest
+//
+//  Created by Melissa Galonsky on 9/11/14.
+//  Copyright (c) 2014 Melissa Galonsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
